a33dee382440e5721e19ef87e6f248a5028a0048
yenitamelia <yenitamelia2@gmail.com>
2022-01-07 11:54:22 +0700

    bikin modals disposisi, tp yg bisa dipencet baru yg baris pertama

--- app/Views/surat/index.php
+++ app/Views/surat/index.php
@@ -2,6 +2,7 @@
 
 <?= $this->section('content'); ?>
 <div class="container mx-auto px-6">
+
     <div class="text-2xl py-5">Data Surat Masuk</div>
     <!-- Alert jika data berhasil ditambahkan -->
     <?php if (session()->getFlashdata('pesan')) : ?>
@@ -14,13 +15,16 @@
         <!-- Tombol Tambah Surat -->
         <a href="/surat/create" class="mb-5 bg-blue-500 rounded-xl text-sm text-white px-3 py-1">Tambah Surat Masuk</a>
     <?php endif; ?>
-    <table id="myTable" class="display" width="100%">
+    <table id="myTable" class="display text-sm" width="100%">
         <thead>
             <tr>
-                <th class="w-1/6">No</th>
-                <th class="w-1/5">Status</th>
+                <th class="w-1/10">No</th>
+                <th class="w-1/6">Tanggal Surat</th>
+                <th class="w-1/6">Tanggal Diterima</th>
+                <th class="w-1/7">Status</th>
                 <th class="w-1/3">Perihal</th>
-                <th class="w-1/5">Aksi</th>
+                <th class="w-1/5">Disposisi Saat Ini</th>
+                <th class="w-1/6">Aksi</th>
                 <!-- <th class="w-1/5">Aksi 2</th> -->
             </tr>
         </thead>
@@ -29,8 +33,16 @@
             <?php foreach ($surat as $s) : ?>
                 <tr>
                     <td><?= $i++; ?></td>
+                    <td>20 Desember 2021</td>
+                    <td>1 Januari 2022</td>
                     <td>Terkirim</td>
                     <td><?= $s['perihal']; ?></td>
+                    <td>
+                        <button class="px-2 py-1 bg-yellow-500 hover:bg-yellow-600 text-gray-100 rounded shadow text-xs" id="disposisi-btn">
+                            Disposisi
+                        </button>
+                        <p>Menunggu Disposisi</p>
+                    </td>
                     <td>
                         <a href="/surat/<?= $s['id']; ?>" class="bg-blue-500 rounded-xl text-sm text-white px-3 py-1">Detail</a>
                     </td>
